ec63bb76799323f7d7f9d1b20c3d067ec444044d
stephenhyde <stephen.hyde@enflick.com>
2017-09-21 23:45:32 -0400

    - Finish script

--- src/main/java/com/mycompany/instagramcredentialvalidation/LoginValidation.java
+++ src/main/java/com/mycompany/instagramcredentialvalidation/LoginValidation.java
@@ -5,22 +5,14 @@
  */
 package com.mycompany.instagramcredentialvalidation;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 /**
  *
  * @author stephen
  */
 public class LoginValidation {
-    PhantomJSDriver driver;  //Ghostdriver
     private boolean result = false;
     private Profile profile;
     
@@ -37,65 +29,41 @@ public class LoginValidation {
             System.out.println("Result1 " + result);
             return;
         }
-        loadLightWeightDriverCustom();
         login();
         System.out.println("Result " + result);
-        driver.quit();
     }
-    
-    private void loadLightWeightDriverCustom() {
-         File PHANTOMJS_EXE = new File("//home/innwadmin/phantomjs/bin/phantomjs");  // Linux File
-        // File PHANTOMJS_EXE = new File("/Users/stephen.hyde/repositories/phantomjs-2.1.1-macosx/bin/phantomjs");
-        // File PHANTOMJS_EXE = new File("C:/Users/stephen/Documents/Instanetwork/Instagram AutoLike/InstagramAutoLike/phantomjs-2.0.0-windows/bin/phantomjs.exe"); // Windows File
-
-        ArrayList<String> cliArgsCap = new ArrayList();
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("phantomjs.binary.path",
-                PHANTOMJS_EXE.getAbsolutePath());
-        caps.setJavascriptEnabled(true);
-        cliArgsCap.add("--proxy=" + profile.getIp() + ":" + profile.getPort()); 
-        if (!profile.getProxyUsername().equalsIgnoreCase("none")) {
-           cliArgsCap.add("--proxy-auth=" + profile.getProxyUsername() + ":" + profile.getProxyPassword());
-        }
-        cliArgsCap.add("--max-disk-cache-size=0");
-        cliArgsCap.add("--disk-cache=false");
-        cliArgsCap.add("--webdriver-loglevel=NONE");
-        caps.setCapability(
-                PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
-        driver = new PhantomJSDriver(caps);
-        driver.manage().window().maximize();
-    }
+   
     private void login(){
-        driver.get("https://www.instagram.com/accounts/login/");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);      
-        List<WebElement> user = driver.findElements(By.xpath("//input[@name='username']"));
-        List<WebElement> pass = driver.findElements(By.xpath("//input[@name='password']"));
-        List<WebElement> login = driver.findElements(By.xpath("//span[1]/button[contains(@class, '_ah57t')]"));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        if(user.size() > 0 && pass.size() > 0 && login.size() > 0){
-            user.get(0).sendKeys(profile.getUsername());
-            pass.get(0).sendKeys(profile.getPassword());
-            sleepExtraPageLoad();
-            login.get(0).click();
-            sleepExtraPageLoad();
+        String s;
+        String scriptResult = null;
+        String location = "/Users/stephen.hyde/repositories/python-instagram-login/startloginscript.py";
+        String proxy = profile.getIp() + ":" + profile.getPort();
+        if (!profile.getProxyUsername().equalsIgnoreCase("none")) {
+            proxy = profile.getProxyUsername() + ":" + profile.getPassword() + "@" + proxy;
         }
-        else{
+        try {
+            Process process = Runtime.getRuntime().exec("python  " + location + " " + profile.getUsername() + " " + profile.getPassword() + " " + proxy);
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            
+            // read the output from the command
+            System.out.println("Output of the script:\n");
+            while ((s = stdInput.readLine()) != null) {
+                scriptResult = s;
+                System.out.println(s);
+            }
+        } catch(IOException e) {
             result = false;
+            return;
         }
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        List<WebElement> name = driver.findElements(By.xpath("//a[contains(@class, '_soakw')]"));
-        result = name.size() > 0;      
+        System.out.println(scriptResult);
+        if (scriptResult != null && scriptResult.equalsIgnoreCase("False")) {
+            result = false;
+        } else {
+            result = true;
+        }
     }
 
     public boolean GetResult(){
         return result;
-    }
-    
-    private void sleepExtraPageLoad() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            System.out.println("Interrupted Exception on sleepDuringWebstaLogin");
-        }
     }
 }
